5d5ba331b47515672ebd71f224ff52df52e0cc18
SferaDev <sferadev@gmail.com>
2014-09-03 10:55:34 +0200

    Typo: Mountain View is default weather city

--- app/src/main/java/com/sferadev/geekthetime/companion/GeekActivity.java
+++ app/src/main/java/com/sferadev/geekthetime/companion/GeekActivity.java
@@ -47,8 +47,8 @@ public class GeekActivity extends PreferenceActivity implements SharedPreference
     }
 
     private void updateSummary() {
-        findPreference("key_custom_tag").setSummary("Custom Tag: " + PreferenceManager.getDefaultSharedPreferences(getContext()).getString("key_custom_tag", "May The Force Be With You"));
-        findPreference("key_location").setSummary("Weather in: " + PreferenceManager.getDefaultSharedPreferences(getContext()).getString("key_location", "Unknown"));
+        findPreference("key_custom_tag").setSummary("Custom Tag: " + PreferenceManager.getDefaultSharedPreferences(getContext()).getString("key_custom_tag", "May The Force Be With You All"));
+        findPreference("key_location").setSummary("Weather in: " + PreferenceManager.getDefaultSharedPreferences(getContext()).getString("key_location", "Mountain View"));
     }
 
     public void onSharedPreferenceChanged(SharedPreferences sp, String key) {
